2429f4e7d8b882a6fb64a27b26d3e79a747ba35a
Alexandra Sarafoglou <alexandra.sarafoglou@gmail.com>
2023-06-19 13:45:44 +0200

    fixed SBC bug; fit model to Sonias data
    fit model to Sonias data: for 1 timepoints
    SBC: posterior contraction and z-score now give expected results

--- dev/simulate_data.R
+++ dev/simulate_data.R
@@ -1,26 +1,33 @@
 library(stringr)
+setwd("~/GitProjects/quant_project1/dev")
 dat <- read.table("../data/exp1-preprocessed-all.csv", header = TRUE, sep = ',')
+# dat1 <- read.table("../data/exp1-preprocessed-all.csv", header = TRUE, sep = ',')
+# dat2 <- dat1
+# dat2$workerid <- factor(dat2$workerid)
+# levels(dat2$workerid) <- paste0(LETTERS, 1:100)[1:length(unique(dat2$workerid))]
+# dat <- rbind(dat1, dat2)
 
-ndatasets      <- 2
-ndat_simulated <- length(list.files('../data/')[grepl('simulated-all', list.files('../data/'))])
-
-ndat_to_simulate <- ndatasets - ndat_simulated
+# ndatasets      <- 1
+# ndat_simulated <- length(list.files('../data/')[grepl('simulated-all-large', list.files('../data/'))])
+ndat_to_simulate <- 2
 
 seeds       <- 2022 + 1:ndat_to_simulate
-nsubjs      <- 8 # 8 participants
-ntrials     <- 3 # 300 data points
+nsubjs      <- 20  # 20 participants
+ntrials     <- 2   # 200 data points per participant; same trials as sonia
 percentages <- c(5, 20, 40, 60, 80, 95)
+#percentages <- seq(0, 100, length.out = 11)
 unique_quants <- unique(dat$qq)
 unique_quants <- str_to_title(unique_quants)
 unique_quants <- str_replace_all(unique_quants, " ", "")
-D <- length(unique_quants)
+# D <- length(unique_quants)
+D <- 1 # "Most"
 
 for(j in 1:ndat_to_simulate){
   
   set.seed(seeds[j])
   
   # inits
-  newsubjs <- sample(unique(dat$workerid), nsubjs, replace = FALSE)
+  newsubjs <- sample(unique(dat$workerid), nsubjs)
   newdat   <- subset(dat, workerid %in% newsubjs) 
   
   # visualize the pattern for each person: fewer than half
@@ -60,13 +67,13 @@ for(j in 1:ndat_to_simulate){
   }
   
   newdat <- do.call("rbind", quants)
-  newdat <- newdat %>% mutate(percent_cat=cut(percent, 
+  newdat <- newdat %>% dplyr::mutate(percent_cat=cut(percent, 
                                               breaks=c(0, 20, 40, 60, 80, 100), 
                                               labels=c("1-20","21-40","41-60", "61-80", "81-100")))
   
   
   write.table(newdat, 
-              paste0("../data/simulated-all-data-", ndat_simulated + j,".csv"), 
+              paste0("../data/simulated-all-data-", j,".csv"), 
               row.names = FALSE, quote = FALSE, sep = ',')
   
 }
